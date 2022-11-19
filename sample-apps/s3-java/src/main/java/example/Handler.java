076af17872aa0361a6bddd2dbe2ff421ee0d6510
Brogzybro <bragehs@hotmail.com>
2022-11-19 20:28:22 +0100

    Added Scraper for 10Times Climate Change Events

--- sample-apps/s3-java/src/main/java/example/Handler.java
+++ sample-apps/s3-java/src/main/java/example/Handler.java
@@ -1,11 +1,13 @@
 package example;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import example.scrapers.Scraper;
+import example.scrapers.TenTimesScraper;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -15,23 +17,40 @@ import software.amazon.awssdk.services.s3.S3Client;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Handler value: example.Handler
 public class Handler implements RequestHandler<ScheduledEvent, String> {
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final Logger logger = LoggerFactory.getLogger(Handler.class);
+    private final List<Scraper> scrapers =  List.of(new TenTimesScraper());
 
     @Override
     public String handleRequest(ScheduledEvent scheduledEvent, Context context) {
 
-        logger.info("EVENT: " + gson.toJson(scheduledEvent));
-        return "Event Processed!";
+        logger.info("Searching for events...");
 
+        var events = new ArrayList<Event>();
+
+        for (var scraper : scrapers) {
+            try {
+                logger.info("Searching events for URL: {}.", scraper.getUrl());
+                var scrapedEvents = scraper.getEvents();
+                logger.info("Found {} events for URL: {}.", scrapedEvents.size(), scraper.getUrl());
+                events.addAll(scrapedEvents);
+            } catch (IOException e) {
+                logger.error("Scraper failed!", e);
+            }
+        }
+
+        logger.info("Finished searching for events!");
+        logger.info("found in total {} events.", events.size());
+
+
+        for (var event : events) {
+            logger.info("Extracted: {}.", event);
+        }
+
+        return "Finished searching for events.";
     }
 
     private InputStream getObject(S3Client s3Client, String bucket, String key) {
@@ -43,7 +62,7 @@ public class Handler implements RequestHandler<ScheduledEvent, String> {
     }
 
     private void putObject(S3Client s3Client, ByteArrayOutputStream outputStream,
-                           String bucket, String key, String imageType) {
+                           String bucket, String key) {
         Map<String, String> metadata = new HashMap<>();
         metadata.put("Content-Length", Integer.toString(outputStream.size()));
 
@@ -62,5 +81,13 @@ public class Handler implements RequestHandler<ScheduledEvent, String> {
             logger.error(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
+    }
+
+    public static void main(String[] args) {
+        var dummyAwsEvent = new ScheduledEvent();
+        dummyAwsEvent.setId("Test");
+
+        Handler handler = new Handler();
+        handler.handleRequest(dummyAwsEvent, null);
     }
 }
